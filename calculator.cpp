878fb592145caedc5de47cd19ee2606e72cc62eb
Connor Keating <cjkeati1@asu.edu>
2019-06-22 19:06:57 -0700

    Added option for working with decimals. Added pi, e, and Rand functions (random funct not finished

--- calculator.cpp
+++ calculator.cpp
@@ -1,13 +1,7 @@
 #include "calculator.h"
 #include "ui_calculator.h"
-
-double calcVal = 0.0;
-bool divTrigger = false;
-bool multTrigger = false;
-bool addTrigger = false;
-bool subTrigger = false;
-double memory = 0.0;
-
+#include "math.h"
+#include "random"
 
 Calculator::Calculator(QWidget *parent) :
     QMainWindow(parent),
@@ -18,11 +12,12 @@ Calculator::Calculator(QWidget *parent) :
     ui->Display->setText(QString::number(calcVal));
     QPushButton *numButtons[10];
     for (int i = 0; i < 10; i++) {
-        QString butName = "Button_"+QString::number(i);
+        QString butName = "Button_" + QString::number(i);
         numButtons[i] = Calculator::findChild<QPushButton *>(butName);
         connect(numButtons[i], SIGNAL(released()), this, SLOT(NumPressed()));
     }
 
+    // Connecting buttons to their respective methods
     connect(ui->Button_Add, SIGNAL(released()), this, SLOT(MathButtonPressed()));
     connect(ui->Button_Subtract, SIGNAL(released()), this, SLOT(MathButtonPressed()));
     connect(ui->Button_Multiply, SIGNAL(released()), this, SLOT(MathButtonPressed()));
@@ -33,20 +28,29 @@ Calculator::Calculator(QWidget *parent) :
     connect(ui->Button_MemClear, SIGNAL(released()), this, SLOT(MemoryClear()));
     connect(ui->Button_MemAdd, SIGNAL(released()), this, SLOT(MemoryAdd()));
     connect(ui->Button_MemGet, SIGNAL(released()), this, SLOT(MemoryGet()));
+    connect(ui->Button_Pi, SIGNAL(released()), this, SLOT(PiPressed()));
+    connect(ui->Button_Euler, SIGNAL(released()), this, SLOT(EulersNumberPressed()));
+    connect(ui->Button_Rand, SIGNAL(released()), this, SLOT(RandomNumberPressed()));
+    connect(ui->Button_DecimalPoint, SIGNAL(released()), this, SLOT(DecimalPointPressed()));
 }
-
 
 Calculator::~Calculator()
 {
     delete ui;
 }
+
 void Calculator::NumPressed(){
-    QPushButton *button = (QPushButton *) sender();
+    QPushButton *button = static_cast<QPushButton *>(sender());
     QString butVal = button->text();
     QString displayVal = ui->Display->text();
-    if((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)){
+    QRegExp reg("[-]?[0-9.]*");
+    if(((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)) && (!decimalTrigger)){
         ui->Display->setText(butVal);
-    } else {
+    }  else if(butVal == "0"){
+        QString newVal = displayVal + butVal;
+        ui->Display->setText(newVal);
+    }
+    else {
         QString newVal = displayVal + butVal;
         double dblNewVal = newVal.toDouble();
         ui->Display->setText(QString::number(dblNewVal, 'g', 16));
@@ -55,14 +59,124 @@ void Calculator::NumPressed(){
 }
 
 void Calculator::MathButtonPressed(){
+    decimalTrigger = false;
+
+    if(!addTrigger && !subTrigger && !multTrigger && !divTrigger){ // No trigger was pushed prior to this one and after the last equal sign
+         GetMathButton();
+         ui->Display->setText("");
+    } else{
+        QString displayVal = ui->Display->text();
+        calcVal = displayVal.toDouble();
+        EqualButton();
+        GetMathButton();
+    }
+}
+
+void Calculator::EqualButton(){
+    double solution = 0.0;
+    QString displayVal = ui->Display->text();
+    double dblDisplayVal = displayVal.toDouble();
+
+    if(addTrigger || subTrigger || multTrigger || divTrigger){
+        if(addTrigger){
+            solution = calcVal + dblDisplayVal;
+        } else if(subTrigger){
+            solution = calcVal - dblDisplayVal;
+        } else if(multTrigger){
+            solution = calcVal * dblDisplayVal;
+        }  else{
+            solution = calcVal / dblDisplayVal;
+        }
+        ui->Display->setText(QString::number(solution));
+    }
+
+    ClearOperatorTriggers();
+
+    decimalTrigger = (solution - static_cast<int>(solution) != 0);
+
+}
+
+void Calculator::ChangeNumberSign(){
+    QString displayVal = ui->Display->text();
+    QRegExp reg("[-]?[0-9.]*");
+    if(reg.exactMatch(displayVal)){
+        double dblDisplayVal = displayVal.toDouble();
+        double dblDisplayValSign = dblDisplayVal * -1;
+        ui->Display->setText(QString::number(dblDisplayValSign));
+    }
+}
+
+void Calculator::ClearButtonPressed(){
+    ui->Display->setText("0");
+    ClearAllTriggers();
+}
+
+void Calculator::MemoryAdd(){
+    QString displayVal = ui->Display->text();
+    memory = displayVal.toDouble();
+}
+
+void Calculator::MemoryClear(){ 
+    memory = 0.0;
+}
+
+void Calculator::MemoryGet(){
+    ui->Display->setText(QString::number(memory));
+    decimalTrigger = (memory - static_cast<int>(memory) != 0);
+
+}
+
+void Calculator::PiPressed(){
+    ui->Display->setText(QString::number(PI));
+    decimalTrigger = true;
+}
+
+void Calculator::EulersNumberPressed(){
+    ui->Display->setText(QString::number(EULER));
+    decimalTrigger = true;
+}
+
+void Calculator::RandomNumberPressed(){
+    double r = (static_cast<double>(rand()) / (INT_MAX)) + 1;
+
+    ui->Display->setText(QString::number(r));
+}
+
+void Calculator::DecimalPointPressed(){
+    QString butVal = ".";
+    QString displayVal = ui->Display->text();
+
+    if((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)){
+        ui->Display->setText("0.");
+        decimalTrigger = true;
+    } else if(!decimalTrigger){
+        QString newVal = displayVal + butVal;
+        ui->Display->setText(newVal);
+        decimalTrigger = true;
+    }
+}
+
+void Calculator::ClearAllTriggers(){
     divTrigger = false;
     multTrigger = false;
     addTrigger = false;
     subTrigger = false;
+    decimalTrigger = false;
+}
+
+void Calculator::ClearOperatorTriggers(){
+    divTrigger = false;
+    multTrigger = false;
+    addTrigger = false;
+    subTrigger = false;
+}
+
+void Calculator::GetMathButton(){
+    ClearOperatorTriggers();
 
     QString displayVal = ui->Display->text();
     calcVal = displayVal.toDouble();
-    QPushButton *button = (QPushButton *) sender();
+    QPushButton *button = static_cast<QPushButton *>(sender());
     QString butVal = button->text();
 
     if(QString::compare(butVal, "/", Qt::CaseInsensitive) == 0){
@@ -74,49 +188,4 @@ void Calculator::MathButtonPressed(){
     } else {
         subTrigger = true;
     }
-    ui->Display->setText("");
-}
-void Calculator::EqualButton(){
-    double solution = 0.0;
-    QString displayVal = ui->Display->text();
-    double dblDisplayVal = displayVal.toDouble();
-    if(addTrigger || subTrigger || multTrigger || divTrigger){
-        if(addTrigger){
-            solution = calcVal + dblDisplayVal;
-        } else if(subTrigger){
-            solution = calcVal - dblDisplayVal;
-        } else if(multTrigger){
-            solution = calcVal * dblDisplayVal;
-        }  else{
-            solution = calcVal / dblDisplayVal;
-        }
-
-        ui->Display->setText(QString::number(solution));
-    }
-}
-void Calculator::ChangeNumberSign(){
-     QString displayVal = ui->Display->text();
-     QRegExp reg("[-]?[0-9.]*");
-     if(reg.exactMatch(displayVal)){
-         double dblDisplayVal = displayVal.toDouble();
-         double dblDisplayValSign = dblDisplayVal * -1;
-         ui->Display->setText(QString::number(dblDisplayValSign));
-     }
-}
-
-void Calculator::ClearButtonPressed(){
-    ui->Display->setText("");
-}
-
-void Calculator::MemoryAdd(){
-     QString displayVal = ui->Display->text();
-     memory = displayVal.toDouble();
-
-}
-void Calculator::MemoryClear(){
-    memory = 0.0;
-
-}
-void Calculator::MemoryGet(){
-    ui->Display->setText(QString::number(memory, 'g', 16));
 }
--- calculator.h
+++ calculator.h
@@ -12,6 +12,15 @@ class Calculator : public QMainWindow
     Q_OBJECT
 
 public:
+    const double PI = 3.141592653589793; // Constant for pi
+    const double EULER = 2.718281828459045; // Constant for Euler's number
+    double calcVal = 0.; // Used to store the number inside the display before a math operator is used
+    double memory = 0.; // Used to store whatever value is inside the display when user wants to add memory (M+)
+    bool divTrigger = false;
+    bool multTrigger = false;
+    bool addTrigger = false;
+    bool subTrigger = false;
+    bool decimalTrigger = false;
     explicit Calculator(QWidget *parent = nullptr);
     ~Calculator();
 
@@ -27,6 +36,13 @@ private slots:
     void MemoryAdd();
     void MemoryClear();
     void MemoryGet();
+    void PiPressed();
+    void EulersNumberPressed();
+    void RandomNumberPressed();
+    void DecimalPointPressed();
+    void ClearAllTriggers();
+    void ClearOperatorTriggers();
+    void GetMathButton();
 };
 
 #endif // CALCULATOR_H
